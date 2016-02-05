f806024b7b09fae2025a1348c3c4c10c6225aad2
Chris Kelly <cpjkelly@gmail.com>
2016-02-05 01:29:34 -0800

    Add limited_infection
    Put infection functions into the Infection namespace
    Add limited_infection
    Add tests for limited_infection

--- lib/infection.rb
+++ lib/infection.rb
@@ -1,22 +1,82 @@
 require_relative "user"
 
-def infect(start_user, site_version)
-  queue = [start_user]
-  discovered = {start_user => true} # nodes that have been enqueued
-  infected = {} # nodes that have been infected
+module Infection
+  # Infect the entire connected component containing the given user
+  # with the given site version through student-coach relationships.
+  def self.infect(start_user, site_version)
+    queue = [start_user]
+    discovered = {start_user => true} # nodes that have already been enqueued
+    infected = {} # nodes that have been infected
 
-  while !queue.empty?
-    curr_user = queue.pop
-    if !infected[curr_user] # only infect and enqueu neighbours of uninfected nodes
-      curr_user.site_version = site_version # set site version
-      infected[curr_user] = true
-      # only push undiscovered neighbours
-      curr_user.edges.each do |edge|
-        if !discovered[edge.to_user]
-          queue.unshift(edge.to_user)
-          discovered[edge.to_user] = true
+    while !queue.empty?
+      curr_user = queue.pop
+      if !infected[curr_user] # only infect and enqueue neighbours of uninfected nodes
+        curr_user.site_version = site_version
+        infected[curr_user] = true
+        curr_user.edges.each do |edge|
+          if !discovered[edge.to_user] # only push undiscovered neighbours
+            queue.unshift(edge.to_user)
+            discovered[edge.to_user] = true
+          end
         end
       end
+    end
+  end
+
+  # Try to infect up to the given limit of users with the given site version.
+  # When the limit is reached, try to minimize the number of student-coach
+  # version differences caused due to stopping the infection.
+  def self.limited_infect(start_user, site_version, limit, buffer)
+    queue = [{to: start_user, from: nil}]
+    discovered = {start_user => true} # nodes that have already been enqueued
+    infected = {} # nodes that have been infected
+
+    while !queue.empty?
+      curr = queue.pop
+      curr_user = curr[:to]
+      if !infected[curr_user] # only infect and enqueue neighbours of uninfected nodes
+        if limit - infected.length <= 1 # if the limit will be reached with this user,
+          curr_user.site_version = site_version
+          infected[curr_user] = true
+          # stop the infection and try to minimize the version differences
+          limit_infection(curr, infected, limit, buffer, site_version)
+          return
+        end
+        curr_user.site_version = site_version
+        infected[curr_user] = true
+        curr_user.edges.each do |edge|
+          if !discovered[edge.to_user]
+            queue.unshift({to: edge.to_user, from: curr_user})
+            discovered[edge.to_user] = true
+          end
+        end
+      end
+    end
+  end
+
+  private
+
+  # Iterate through the current_user's uninfected siblings,
+  # and naively infect them if doing so will prevent version differences.
+  def self.limit_infection(curr_hash, infected, limit, buffer, site_version)
+    uninfected_siblings = curr_hash[:from].edges
+                         .map { |edge| edge.to_user }
+                         .select { |user| !infected[user] }
+
+    diffs = uninfected_siblings.map do |sib|
+      adjacent_users = sib.edges.map { |edge| edge.to_user }
+      diffs_if_infect = adjacent_users.select { |user| !infected[user] }.count
+      diffs_if_do_not_infect = adjacent_users.select { |user| !!infected[user] }.count
+      diff = diffs_if_infect - diffs_if_do_not_infect
+      { user: sib, diff: diff }
+    end
+
+    diff = diffs.sort_by {|diff_hash| diff_hash[:diff]}
+    buffer += 1
+    diff.each do |diff| # infect all siblings for which infection will reduce version differences
+      break if buffer <= 0 || diff[:diff] <= 0
+      diff[:user].site_version = site_version
+      infected[diff[:user]] = true
     end
   end
 end
--- test/infection_test.rb
+++ test/infection_test.rb
@@ -1,5 +1,5 @@
 require "minitest/autorun"
-require_relative "../infection"
+require_relative "../lib/infection"
 
 class InfectionTest < Minitest::Test
 
@@ -10,7 +10,7 @@ class InfectionTest < Minitest::Test
     u1.add_student u2
     u2.add_coach u1
 
-    infect(u1, "v0.1.1")
+    Infection.infect(u1, "v0.1.1")
 
     assert u1.site_version == "v0.1.1"
   end
@@ -22,7 +22,7 @@ class InfectionTest < Minitest::Test
     u1.add_student u2
     u2.add_coach u1
 
-    infect(u1, "v0.1.1")
+    Infection.infect(u1, "v0.1.1")
 
     assert u1.site_version == "v0.1.1" && u2.site_version == "v0.1.1"
   end
@@ -31,7 +31,7 @@ class InfectionTest < Minitest::Test
     u1 = User.new
     u2 = User.new
 
-    infect(u1, "v0.1.1")
+    Infection.infect(u1, "v0.1.1")
 
     refute u2.site_version == "v0.1.1"
   end
@@ -48,10 +48,107 @@ class InfectionTest < Minitest::Test
     u3.add_student u1
     u1.add_coach u3
 
-    infect(u1, "v0.1.1")
+    Infection.infect(u1, "v0.1.1")
 
     assert u1.site_version == "v0.1.1"
     assert u2.site_version == "v0.1.1"
     assert u3.site_version == "v0.1.1"
+  end
+
+  def test_limited_infection_infects_first_user
+    u1 = User.new
+    u2 = User.new
+
+    u1.add_student u2
+    u2.add_coach u1
+
+    Infection.limited_infect(u1, "v0.1.1", limit=2, buffer=1)
+
+    assert u1.site_version == "v0.1.1"
+  end
+
+  def test_limited_infection_limited_infection_infects_next_user
+    u1 = User.new
+    u2 = User.new
+
+    u1.add_student u2
+    u2.add_coach u1
+
+    Infection.limited_infect(u1, "v0.1.1", 4, 0)
+
+    assert u1.site_version == "v0.1.1" && u2.site_version == "v0.1.1"
+  end
+
+  def test_limited_infection_does_not_infect_unconnected_user
+    u1 = User.new
+    u2 = User.new
+
+    Infection.limited_infect(u1, "v0.1.1", 4, 0)
+
+    refute u2.site_version == "v0.1.1"
+  end
+
+  def test_limited_infection_circular_relationship
+    u1 = User.new
+    u2 = User.new
+    u3 = User.new
+
+    u1.add_student u2
+    u2.add_coach u1
+    u2.add_student u3
+    u3.add_coach u2
+    u3.add_student u1
+    u1.add_coach u3
+
+    Infection.limited_infect(u1, "v0.1.1", 4, 0)
+
+    assert u1.site_version == "v0.1.1"
+    assert u2.site_version == "v0.1.1"
+    assert u3.site_version == "v0.1.1"
+  end
+
+  def test_limited_infect_infects_up_to_limit
+    u1 = User.new
+    u2 = User.new
+    u3 = User.new
+    u4 = User.new
+    u5 = User.new
+
+    u1.add_student u2
+    u2.add_coach u1
+    u2.add_student u3
+    u3.add_coach u2
+    u3.add_student u4
+    u4.add_coach u3
+    u4.add_student u5
+    u5.add_coach u4
+
+    Infection.limited_infect(u1, "v0.1.1", 2, 0)
+    assert u1.site_version == "v0.1.1"
+    assert u2.site_version == "v0.1.1"
+    refute u3.site_version == "v0.1.1"
+  end
+
+  def test_limited_infect_infects_up_to_limit_plus_buffer
+    u1 = User.new
+    u2 = User.new
+    u3 = User.new
+    u4 = User.new
+    u5 = User.new
+
+    u1.add_student u2
+    u2.add_coach u1
+    u2.add_student u3
+    u3.add_coach u2
+    u3.add_student u4
+    u4.add_coach u3
+    u4.add_student u5
+    u5.add_coach u4
+
+    Infection.limited_infect(u1, "v0.1.1", 3, 1)
+    assert u1.site_version == "v0.1.1"
+    assert u2.site_version == "v0.1.1"
+    assert u3.site_version == "v0.1.1"
+    refute u4.site_version == "v0.1.1"
   end
 end
